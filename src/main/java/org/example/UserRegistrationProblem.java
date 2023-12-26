810ede04baf89110a4497bfa601798d1adb0e3ac
ARJUN YEDALE <arjunyedale8748@gmail.com>
2023-12-26 21:09:01 +0530

    added welcome message

---
+++ src/main/java/org/example/UserRegistrationProblem.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.regex.Pattern;
+
+public class UserRegistrationProblem {
+    public static void main(String[] args) {
+        System.out.println("welcome to user registration");
+    }
+}
